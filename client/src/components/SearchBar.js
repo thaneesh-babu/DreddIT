ccfdb6054b9e3237e40537461af06441392cd86e
Thaneesh Babu <63804742+thaneesh-babu@users.noreply.github.com>
2022-02-22 00:12:27 -0500

    handle input change

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,10 +1,15 @@
 import { Input, InputGroup, InputLeftAddon, Button } from '@chakra-ui/react'
+import { useState } from 'react'
 
 const SearchBar = () => {
+	const [input, setInput] = useState('Search subreddit')
+
+	const handleInputChange = (e) => setInput(e.target.value)
+
 	return (
 		<InputGroup size='sm'>
 	    <InputLeftAddon children='r/' />
-	    <Input placeholder='Search subreddit' />
+	    <Input value={input} onChange={handleInputChange} />
 		<Button h='1.75rem' size='sm'>
           Search
         </Button>	  	
